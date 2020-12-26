91700bfeb752cb2a88d81ca27de409542dcf80af
Mizanur Rahman <56992426+rajwebhost@users.noreply.github.com>
2020-12-26 21:38:34 +0600

    default-skeleton-raj

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en-us" class="no-js">
+  <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    
+    <title>We are Coming Soon - Hosted by Raj Web Host Ltd
+    </title>
+    <meta name="description" content="This website is hosted by Raj Web Host Ltd, one of the best & reliable premium web hosting service provider in Bangladesh.">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="RajWebHost">
+    <!-- ================= Favicon ================== -->
+    <!-- Standard -->
+    <link rel="icon" type="image/png" sizes="32x32" href="https://www.rajwebhost.com/favicon/favicon-32x32.png">
+    <link rel="shortcut icon" href="https://www.rajwebhost.com/favicon/favicon-32x32.png">
+    <!-- Retina iPad Touch Icon-->
+    <!--<link rel="apple-touch-icon" sizes="144x144" href="https://www.RajWebHost.com/images/favicon.ico">-->
+    <!-- Retina iPhone Touch Icon-->
+    <!--<link rel="apple-touch-icon" sizes="114x114" href="https://www.RajWebHost.com/images/favicon.ico">-->
+    <!-- Standard iPad Touch Icon--> 
+    <!--<link rel="apple-touch-icon" sizes="72x72" href="https://www.RajWebHost.com/images/favicon.ico">-->
+    <!-- Standard iPhone Touch Icon--> 
+    <!--<link rel="apple-touch-icon" sizes="57x57" href="https://www.RajWebHost.com/images/favicon.ico">-->
+    <!-- ============== Resources style ============== -->
+    <link rel="stylesheet" href="css/style-minimal.css" />
+    <!-- Modernizr runs quickly on page load to detect features -->
+    <script src="js/modernizr.custom.js">
+    </script>
+  </head>
+  <body>
+    <!-- Page preloader -->
+    <div id="loading">
+      <div id="preloader">
+        <span>
+        </span>
+        <span>
+        </span>
+      </div>
+    </div>
+    <!-- Overlay -->
+    <div class="global-overlay">
+      <div class="overlay skew-part">
+      </div>
+    </div>
+    <!-- START - Home/Left Part -->
+    <section id="left-side" class="minimal-phone">
+      <!-- Your logo -->
+      <a href="https://www.rajwebhost.com" target="_blank">
+      <img src="https://www.rajwebhost.com/images/logo.svg" alt="RajWebHost" class="brand-logo" />
+ 	 </a>
+      <div class="content">
+        <br />
+        <br />
+        <br />
+        <br>
+        <h1 class="text-intro opacity-0"><?php
+        $domain_name = $_SERVER['SERVER_NAME'];
+        
+        echo $domain_name ; ?></h1>
+        <h1>is coming soon
+        </h1>
+		<h2>Server IP is: 
+		<?php
+        $server_ip = $_SERVER['SERVER_ADDR'];
+        
+        echo $server_ip ; 
+		?>
+		</h2>
+		<h2>Your IP address is: 
+		<?php
+        $visitor_ip = $_SERVER['REMOTE_ADDR'];
+        
+        echo $visitor_ip ; 
+		?>
+		</h2>
+        <!--<h2><div id="getting-started"></div></h2>-->
+        <h2 class="text-intro opacity-0">Domain / Hosting Service Provided by 
+          <a class="raj" href="https://www.RajWebHost.com/" target="_blank">RAJ WEB HOST
+          </a>
+          <br />
+        </h2>
+        <br />
+        Office Address:
+        <br>
+        <br>
+        RAJ WEB HOST LTD.
+        <br />
+        H#333/01 (4th Floor), Hatem Khan Choto Masjid Road, <br>
+        (In front of DBH Foundation School)<br>
+        Boalia, Ghoramara, Rajshahi -  6100<br>
+          
+        <br />
+        <br />
+        Sales: 
+        <a href="tel:+8801714825865">+88 01714 825 865
+        </a> 
+        <br />
+        Available during 08:00 AM to 11:59 PM (GMT +06:00)
+        <br />
+        <br />
+        Website: 
+        <a href="https://www.RajWebHost.com/" target="_blank">www.rajwebhost.com
+        </a> 
+        <br>
+        Email: 
+        <a href="admin@rajwebhost.com">admin@rajwebhost.com
+        </a>
+        <br />
+      </div>
+      <!-- Social icons -->
+      <div class="social-icons">
+        <a href="https://www.facebook.com/rajwebhost/">
+          <i class="fa fa-facebook">
+          </i>
+        </a>
+        <a href="https://www.twitter.com/rajwebhost">
+          <i class="fa fa-twitter">
+          </i>
+        </a>
+        <a href="https://plus.google.com/+Rajwebhost">
+          <i class="fa fa-google-plus">
+          </i>
+        </a>
+        <a href="https://www.linkedin.com/company/raj-web-host">
+          <i class="fa fa-linkedin">
+          </i>
+        </a>
+      </div>
+    </section>
+    <!-- END - Home/Left Part -->
+    <!-- ///////////////////\\\\\\\\\\\\\\\\\\\ -->
+    <!-- ********** Resources jQuery ********** -->
+    <!-- \\\\\\\\\\\\\\\\\\\/////////////////// -->
+    <!-- * Libraries jQuery, Easing and Bootstrap - Be careful to not remove them * -->
+    <script src="js/jquery.min.js">
+    </script>
+    <script src="js/jquery.easings.min.js">
+    </script>
+    <script src="js/bootstrap.min.js">
+    </script>
+    <!-- PhotoSwipe Core JS file -->
+    <script src="js/velocity.min.js">
+    </script> 
+    <!-- PhotoSwipe UI JS file -->
+    <script src="js/velocity.ui.min.js">
+    </script> 
+    <!-- Newsletter plugin -->
+    <script src="js/notifyMe.js">
+    </script>
+    <!-- Slideshow/Image plugin -->
+    <script src="js/vegas.js">
+    </script>
+    <!-- Scroll plugin -->
+    <script src="js/jquery.mousewheel.js">
+    </script>
+    <!-- Custom Scrollbar plugin -->
+    <script src="js/jquery.mCustomScrollbar.js">
+    </script>
+    <!-- Popup Newsletter Form -->
+    <script src="js/classie.js">
+    </script>
+    <script src="js/dialogFx.js">
+    </script>
+    <!-- Countdown plugin -->
+    <script src="js/jquery.countdown.js">
+    </script>
+    <script>
+      $("#getting-started")
+      // Year/Month/Day Hour:Minute:Second
+        .countdown("2020/01/01 15:30:30", function(event) {
+        $(this).text(
+          event.strftime('%D Days %Hh %Mm %Ss')
+        );
+      }
+                  );
+    </script>
+    <!-- Main JS File -->
+    <script src="js/main.js">
+    </script>
+    <!--[if lt IE 10]><script type="text/javascript" src="js/placeholder.js"></script><![endif]-->
+  </body>
+</html>
